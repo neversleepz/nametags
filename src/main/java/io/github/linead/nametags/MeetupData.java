d54a9921e58ea8d5b4cb8f21dae3b8fc60981d4c
Kon Soulianidis <kon@outrospective.org>
2017-04-21 21:14:46 +1000

    Inject the error logging rest template into the MeetupData service.

--- src/main/java/io/github/linead/nametags/MeetupData.java
+++ src/main/java/io/github/linead/nametags/MeetupData.java
@@ -2,11 +2,13 @@ package io.github.linead.nametags;
 
 import io.github.linead.nametags.domain.Event;
 import io.github.linead.nametags.domain.Members;
+import io.github.linead.nametags.domain.Members.Member;
 import io.github.linead.nametags.domain.Rsvps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -14,20 +16,15 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 @Component
 public class MeetupData {
 
-    @Autowired
-    Environment env;
-
-    @Value("${meetup-api.key}")
-    private String key;
-
-    @Value("${meetup-url-path}")
-    private String meetupPath;
+    private static final Logger LOG = LoggerFactory.getLogger(MeetupData.class);
 
     private static final String EVENT_URL = "https://api.meetup.com/{meetup_url_path}/events?scroll=recent_past&photo-host=public&page=20&key={key}";
 
@@ -37,38 +34,40 @@ public class MeetupData {
 
     private static final String HOSTS_URL = "https://api.meetup.com/{meetup_url_path}/events/{event_id}/hosts?key={key}";
 
+    @Value("${meetup-api.key}")
+    private String key;
 
-    public String getKey() { return key; }
+    @Value("${meetup-url-path}")
+    private String meetupPath;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MeetupData(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public Event[] getNextMeetups() {
-
-        RestTemplate restTemplate = new RestTemplate();
         Map<String, Object> params = new HashMap<>();
         params.put("key", getKey());
         params.put("meetup_url_path", getMeetupPath());
+
         ResponseEntity<Event[]> responseEntity = restTemplate.getForEntity(EVENT_URL, Event[].class, params);
-        Event[] events = responseEntity.getBody();
-
-        return events;
-
+        return responseEntity.getBody();
     }
 
     public Rsvps getRsvps(String eventId) {
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> params = new HashMap<String, Object>();
+        Map<String, Object> params = new HashMap<>();
         params.put("event_id", eventId);
         params.put("key", getKey());
 
         ResponseEntity<Rsvps> responseEntity = restTemplate.getForEntity(RSVP_URL, Rsvps.class, params);
-        Rsvps rsvps = responseEntity.getBody();
-        
-        return rsvps;
+        return responseEntity.getBody();
     }
 
-    @Cacheable("members")
-    public Map<String, Members.Member> getMembers(String groupId) {
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> params = new HashMap<String, Object>();
+    @Cacheable(value = "members")
+    public Map<String, Member> getMembers(String groupId) {
+        Map<String, Object> params = new HashMap<>();
         int page = 0;
         params.put("group_id", groupId);
         params.put("key", getKey());
@@ -77,49 +76,46 @@ public class MeetupData {
         ResponseEntity<Members> responseEntity = restTemplate.getForEntity(MEMBERS_URL, Members.class, params);
         Members members = responseEntity.getBody();
 
-        HashMap<String, Members.Member> memberMap = new HashMap<String, Members.Member>();
-
-        memberMap.putAll(Arrays.asList(members.getResults())
-                .stream().filter(s -> s.getId() != null)
-                .collect(
-                        Collectors.toMap(Members.Member::getId, Function.identity()
-                        )));
+        Map<String, Member> memberMap = nextPageOf(members);
 
         //read all pages
         while(!StringUtils.isEmpty(members.getMeta().getNext())) {
-            String nextUrl = members.getMeta().getNext();
+            LOG.trace("nextUrl = {}", members.getMeta().getNext());
             params.put("page", ++page);
             responseEntity = restTemplate.getForEntity(MEMBERS_URL, Members.class, params);
             members = responseEntity.getBody();
 
-            memberMap.putAll(Arrays.asList(members.getResults())
-                    .stream().filter(s -> s.getId() != null)
-                    .collect(
-                            Collectors.toMap(Members.Member::getId, Function.identity()
-                            )));
+            memberMap.putAll(nextPageOf(members));
         }
-
-
 
         return memberMap;
     }
 
-
-    public String getMeetupPath() {
-        return meetupPath;
+    private Map<String, Member> nextPageOf(Members members) {
+        return Stream.of(members.getResults())
+                .filter(m -> m.getId() != null)
+                .collect(toMap(Member::getId, Function.identity()));
     }
 
     public Set<String> getHosts(String eventId) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> params = new HashMap<String, Object>();
+        Map<String, Object> params = new HashMap<>();
 
         params.put("meetup_url_path", getMeetupPath());
         params.put("event_id", eventId);
         params.put("key", getKey());
 
-        ResponseEntity<Members.Member[]> responseEntity = restTemplate.getForEntity(HOSTS_URL, Members.Member[].class, params);
+        ResponseEntity<Member[]> responseEntity = restTemplate.getForEntity(HOSTS_URL, Member[].class, params);
 
-        return Stream.of(responseEntity.getBody()).map(Members.Member::getId).collect(Collectors.toSet());
+        return Stream.of(responseEntity.getBody())
+                .map(Member::getId)
+                .collect(toSet());
+    }
+
+
+    public String getKey() { return key; }
+
+    public String getMeetupPath() {
+        return meetupPath;
     }
 }
